92666307f4b4c08ac7c218ccb44469cf9160d57e
BORA <a52447879@gmail.com>
2021-12-22 10:26:19 +0900

    깃허브 배포 설정 완료

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,21 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
 function Logo() {
     return (
         <h1>
-            <strong>DCODELAB2222</strong><br />
+            <strong>DCODELAB</strong><br />
             <span>UI/UX DESIGN&DEVELOPMENT</span>
         </h1>
     )
 }
 
-const Menu = () => {
+function Menu() {
     return (
+
         <a href="#" className="menu">
             <i className="fas fa-bars"></i>
         </a>
-    )
+    );
 }
 
 export { Logo, Menu };
